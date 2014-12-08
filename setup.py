d0506b353f8b5efe1b5bfd59dee54a5dd700e538
Tony S Yu <tsyu80@gmail.com>
2014-12-08 00:57:00 -0600

    Improve setup and requirements

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from os.path import join
+
+from pip.req import parse_requirements
 from setuptools import setup, find_packages
 
 
 package_name = 'mpl_style_gallery'
+
+install_reqs = list(parse_requirements('requirements.txt'))
+requirements = [str(ir.req) for ir in install_reqs]
+
 info = {}
 execfile(join(package_name, '__init__.py'), info)
 
@@ -15,8 +21,9 @@ setup(
     maintainer='Tony S. Yu',
     maintainer_email='tsyu80@gmail.com',
     description='Gallery for Matplotlib stylesheets',
+    url='https://github.com/tonysyu/matplotlib-style-gallery',
     include_package_data=True,
-    install_requires=[],
-    license='BSD',
+    install_requires=requirements,
+    license='Modified BSD',
     packages=find_packages(),
 )

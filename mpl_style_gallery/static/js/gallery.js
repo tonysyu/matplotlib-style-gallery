33b4f01b303db328dc834468f282b4ce75826df0
Tony S Yu <tsyu80@gmail.com>
2015-01-03 18:43:24 -0600

    Group lightbox into a constructor

--- mpl_style_gallery/static/js/gallery.js
+++ mpl_style_gallery/static/js/gallery.js
@@ -1,42 +1,48 @@
 /*jslint browser:true */
-/*global $, jQuery, alert*/
+/*global $, Spinner*/
 
 (function mplStyleGallery() {
     'use strict';
 
-    var ESCAPE_KEY = 27,
-        imageDatabase = {};
+    function createLightbox($el) {
+        var ESCAPE_KEY = 27,
+            lightboxFacade;
+
+        // Press ESCAPE to hide the lightbox window.
+        $(document).keyup(function (event) {
+            if (event.keyCode === ESCAPE_KEY) {
+                $el.hide();
+            }
+        });
+
+        lightboxFacade = {
+            display : function (event) {
+                // Replace anchor targets with lightbox display
+
+                var imageHref = $(this).attr('href');  // clicked link target
+
+                // Prevent default (hyperlink) action
+                event.preventDefault();
+
+                // Set lightbox image source to the clicked image.
+                $('#lightbox img').attr('src', imageHref);
+
+                $el.show();
+            }
+        };
+        return lightboxFacade;
+    }
 
     $(document).ready(function () {
-        // Replace anchor targets with lighbox display
 
-        var $lightbox = $('#lightbox');
+        var lightbox = createLightbox($('#lightbox'));
 
         $('input').click(function (event) {
             $(this).after(new Spinner({}).spin().el);
         });
 
         // click on any lightbox-viewable to open lightbox window.
-        $('a.lightbox-viewable').click(function (event) {
-
-            var imageHref = $(this).attr('href');  // clicked link target
-
-            // Prevent default (hyperlink) action
-            event.preventDefault();
-
-            // Set lightbox image source to the clicked image.
-            $('#content img').attr('src', imageHref);
-
-            $lightbox.show();
-
-        });
-
-        // Press ESCAPE to hide the lightbox window.
-        $(document).keyup(function (event) {
-            if (event.keyCode == ESCAPE_KEY) {
-                $lightbox.hide();
-            }
-        });
+        $('a.lightbox-viewable').click(lightbox.display);
 
     });
 }());

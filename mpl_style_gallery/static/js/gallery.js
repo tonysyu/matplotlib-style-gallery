5719e6bf21511b5fbd7d971ea05fe98c3febb81c
Tony S Yu <tsyu80@gmail.com>
2016-03-27 22:21:42 -0500

    Add arrow-key navigation for gallery table

--- mpl_style_gallery/static/js/gallery.js
+++ mpl_style_gallery/static/js/gallery.js
@@ -10,30 +10,91 @@
      * @param {JQuery} Target lightbox div containing single `img` element.
      */
     function createLightbox($el) {
-        var ESCAPE_KEY = 27,
-            lightboxFacade;
+        var SELECTED = null,
+            KEY_ESCAPE = 27,
+            KEY_LEFT_ARROW = 37,
+            KEY_UP_ARROW = 38,
+            KEY_RIGHT_ARROW = 39,
+            KEY_DOWN_ARROW = 40,
+            ARROW_KEYS = [KEY_LEFT_ARROW, KEY_UP_ARROW,
+                          KEY_RIGHT_ARROW, KEY_DOWN_ARROW];
 
-        // Press ESCAPE to hide the lightbox window.
+        function getElementAtIndex(iRow, iCol) {
+            return $("[data-icol='" + iCol + "'][data-irow='" + iRow + "']");
+        }
+
+        function isArrowKey(keyCode) {
+            return ARROW_KEYS.indexOf(keyCode) !== -1;
+        }
+
+        function displayImage($elImage) {
+            SELECTED = {
+                iCol: parseInt($elImage.attr('data-icol'), 10),
+                iRow: parseInt($elImage.attr('data-irow'), 10),
+                style: $elImage.attr('data-style'),
+                script: $elImage.attr('data-script'),
+            };
+            var imagePath = $elImage.attr('href');
+            $el.children('img').attr('src', imagePath);
+        }
+
+        function incrementAndWrap(value, increment, max) {
+            var newValue = (value + increment) % max;
+            if (newValue < 0) {
+                newValue += max;
+            }
+            return newValue;
+        }
+
+        // Update lightbox impage based on arrow-keys.
+        function updateLightboxImage(keyCode) {
+            // Subtract 1 to remove header row and column.
+            var table = document.getElementById("gallery"),
+                nRows = table.rows.length - 1,
+                nCols = table.rows[0].cells.length - 1;
+
+            if (keyCode === KEY_LEFT_ARROW) {
+                SELECTED.iCol = incrementAndWrap(SELECTED.iCol, -1, nCols);
+            } else if (keyCode === KEY_RIGHT_ARROW) {
+                SELECTED.iCol = incrementAndWrap(SELECTED.iCol, 1, nCols);
+            }else  if (keyCode === KEY_DOWN_ARROW) {
+                SELECTED.iRow = incrementAndWrap(SELECTED.iRow, 1, nRows);
+            } else if (keyCode === KEY_UP_ARROW) {
+                SELECTED.iRow = incrementAndWrap(SELECTED.iRow, -1, nRows);
+            }
+
+            var $elImage = getElementAtIndex(SELECTED.iRow, SELECTED.iCol);
+            SELECTED.style = $elImage.attr('data-style');
+            SELECTED.script = $elImage.attr('data-script');
+
+            displayImage($elImage);
+        }
+
+        $(document).keydown(function (event) {
+            if (SELECTED !== null && isArrowKey(event.keyCode)) {
+                event.preventDefault(); // Prevent default (scroll) action
+            }
+        });
+
         $(document).keyup(function (event) {
-            if (event.keyCode === ESCAPE_KEY) {
+            // Press ESCAPE to hide the lightbox window.
+            if (event.keyCode === KEY_ESCAPE) {
+                SELECTED = null;
                 $el.hide();
+            } else if (SELECTED !== null && isArrowKey(event.keyCode)) {
+                updateLightboxImage(event.keyCode);
             }
         });
 
         // The public interface for lighbox.
-        lightboxFacade = {
+        var lightboxFacade = {
 
             // Replace anchor targets with lightbox display.
             display : function (event) {
 
-                var imageHref = $(this).attr('href');  // clicked link target
-
                 // Prevent default (hyperlink) action
                 event.preventDefault();
-
-                // Set lightbox image source to the clicked image.
-                $el.children('img').attr('src', imageHref);
-
+                displayImage($(this));
                 $el.show();
             }
         };
@@ -41,7 +102,6 @@
     }
 
     $(document).ready(function () {
-
         var lightbox = createLightbox($('#lightbox'));
 
         // Stylesheet input fires spinner since there's a noticeable delay.
